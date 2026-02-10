eb74825e4948f9006338a05ffab652c55167ec72
Reva GDA <reva@gaiada.com>
2026-02-10 11:20:11 +0800

    fix: admin routes with base url

--- frontend/vite.config.ssr.ts
+++ frontend/vite.config.ssr.ts
@@ -1,30 +1,43 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 import svgr from "vite-plugin-svgr"
 
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        icon: true,
-        exportType: "named",
-        namedExport: "ReactComponent"
-      }
-    })
-  ],
-  build: {
-    ssr: true,
-    // ssr: "src/entry-server.tsx",
-    outDir: "dist/server",
-    rollupOptions: {
-      input: {
-        front: "src/entry-server.tsx",
-        admin: "src/entry-server-admin.tsx"
-      }
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "")
+
+    return {
+        plugins: [
+            react(),
+            svgr({
+                svgrOptions: {
+                    icon: true,
+                    exportType: "named",
+                    namedExport: "ReactComponent"
+                }
+            })
+        ],
+
+        base: env.VITE_BASE_PATH,
+
+        build: {
+            ssr: true,
+            outDir: "dist/server",
+            rollupOptions: {
+                input: {
+                    front: "src/entry-server.tsx",
+                    admin: "src/entry-server-admin.tsx"
+                }
+            }
+        },
+
+        ssr: {
+            noExternal: [
+                "react-helmet-async",
+                "quill",
+                "react-quill",
+                "react-quill-new",
+                "stream"
+            ]
+        }
     }
-  },
-  ssr: {
-    noExternal: ["react-helmet-async", "quill", "react-quill", "react-quill-new", "stream"]
-  }
 })
