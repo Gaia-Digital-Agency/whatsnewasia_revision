2ba17870d478535cb3cc5550cd8b3a7eabf8c719
Reva GDA <reva@gaiada.com>
2026-02-13 13:35:15 +0800

    add: query ads for specific location

--- frontend/src/hooks/useAdvertisement.ts
+++ frontend/src/hooks/useAdvertisement.ts
@@ -1,9 +1,31 @@
 import { useAds } from "../context/AdsContext"
+import { useRoute } from "../context/RouteContext";
+import { getTemplateByUrl } from "../services/template.service";
 
 
 const useAdvertisement = () => { 
     const {initialAds} = useAds()
-    return {slot: initialAds?.slot, clientId: initialAds.clientid ?? false}
+    const {actualRoute} = useRoute()
+
+    const getAdsByUrl = async (type: "leaderboard" | "mrec" | "mrec2") => {
+        let url = []
+        if(actualRoute.country) url.push(actualRoute.country.slug)
+        if(actualRoute.city) url.push(actualRoute.city.slug)
+        if(actualRoute.region) url.push(actualRoute.region.slug)
+
+        for(let i = 0; i < url.length; i++) {
+            const _url = url.join('/')
+            const get = await getTemplateByUrl(`/ads/${type}/${_url}`)
+            if(get?.status_code == 200 && get?.data?.content) {
+                return get?.data?.content
+            }
+            url.pop()
+        }
+        return ""
+        
+    }
+
+    return {slot: initialAds?.slot, clientId: initialAds.clientid ?? false, getAdsByUrl}
 };
 
 export default useAdvertisement
